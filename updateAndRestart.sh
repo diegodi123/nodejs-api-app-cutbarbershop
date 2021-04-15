427579f2603d81b17970c7e09a63cbce64e63bbf
Diego Macedo <diegodi123@gmail.com.br>
2021-04-15 19:33:04 -0300

    add Develop  enveriment into digital ocean

--- updateAndRestart.sh
+++ updateAndRestart.sh
@@ -6,31 +6,26 @@ ENVIRONMENT=$ENVIRONMENT
 CI_ENVIRONMENT_NAME=$CI_ENVIRONMENT_NAME
 
 echo "Clone the repository into $CI_ENVIRONMENT_NAME"
-exit
+git pull origin $ENVIRONMENT
 
 echo "change dir for aplication"
-cd nodejs-api-app-cutbarbershop/
-
-
-echo "Clone the repository ENVIRONMENT=$ENVIRONMENT"
-exit
-git pull origin $ENVIRONMENT
+cd nodejs-api-app-cutbarbershop-$CI_ENVIRONMENT_NAME/
 
 echo "Local:"
 pwd
 
 echo "Stoping containers"
-docker-compose down
+docker-compose -f docker-compose.$CI_ENVIRONMENT_NAME.yml down
 
 echo "Build image"
-sudo docker build -t diegodi123/cutbarbershop-dockerizado:latest .
+sudo docker build -t diegodi123/cutbarbershop-dockerizado-$CI_ENVIRONMENT_NAME:latest .
 
 echo "Prune images and Containers"
 docker container prune --force
 docker image prune --force
 
 echo "Starting containers"
-docker-compose up -d
+docker-compose -f docker-compose.$CI_ENVIRONMENT_NAME.yml up -d
 
 echo 'Upgrade migrations'
 docker-compose run nodejs yarn typeorm migration:run
